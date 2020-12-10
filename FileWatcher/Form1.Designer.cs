4fbf8095a50c0ec67f6cd69e6a55e5b6a0ba13fd
Erik JS <erikjs_br@hotmail.com>
2020-12-10 20:59:24 -0300

    First commit

---
+++ FileWatcher/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FileWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(594, 121);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 150);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(178, 41);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "SELECT FILE";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(223, 150);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(178, 41);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(429, 150);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(178, 41);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Log:";
+            // 
+            // rtb
+            // 
+            this.rtb.BackColor = System.Drawing.Color.Black;
+            this.rtb.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb.ForeColor = System.Drawing.Color.Lime;
+            this.rtb.Location = new System.Drawing.Point(15, 228);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(591, 245);
+            this.rtb.TabIndex = 5;
+            this.rtb.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 485);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileWatcher";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtb;
+    }
+}
+
---
+++ FileWatcher/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileWatcher
+{
+    public partial class Form1 : Form
+    {
+
+        private static Form1 mainForm;
+
+        private static bool stopFlag = true;
+
+        private static List<Thread> lstThreads = new List<Thread>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            mainForm = this;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+            listBox1.Items.Add(ofd.FileName);
+        }
+
+
+        public static void LogText(string text, Color color)
+        {
+            mainForm.Invoke(new Action(() =>
+            {
+                mainForm.rtb.SelectionStart = mainForm.rtb.TextLength;
+                mainForm.rtb.SelectionLength = 0;
+                mainForm.rtb.SelectionColor = color;
+                mainForm.rtb.AppendText(text + Environment.NewLine);
+                mainForm.rtb.SelectionBackColor = mainForm.rtb.BackColor;
+                mainForm.rtb.SelectionColor = mainForm.rtb.ForeColor;
+                mainForm.rtb.SelectionStart = mainForm.rtb.TextLength;
+                mainForm.rtb.SelectionLength = 0;
+                mainForm.rtb.ScrollToCaret();
+            }));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnSelect.Enabled = false;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            stopFlag = false;
+
+            lstThreads.Clear();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                var t = new Thread(FileWatchThread);
+                t.Start(listBox1.Items[i]);
+                lstThreads.Add(t);
+            }
+        }
+
+        public static void FileWatchThread(object param)
+        {
+            string fullfilename = (string)param;
+            FileSystemWatcher2 fsw = null;
+
+            try
+            {
+                var dir = Path.GetDirectoryName(fullfilename);
+                var backupdir = Path.Combine(dir, "FileWatcherBackups");
+                Directory.CreateDirectory(backupdir);
+
+                fsw = new FileSystemWatcher2(dir);
+                fsw.Filter = Path.GetFileName(fullfilename);
+                fsw.NotifyFilter = NotifyFilters.LastWrite;
+                fsw.Changed += FileSystemWatcher_Changed;
+
+                fsw.EnableRaisingEvents = true;
+
+                while (!stopFlag)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogText("***FileWatchThread EXCEPTION***\n" + fullfilename + " >> " + (fsw != null).ToString() + "\n" + GetExceptionMessage(ex), Color.LightCoral);
+            }
+            finally
+            {
+                if (fsw != null)
+                {
+                    fsw.EnableRaisingEvents = false;
+                    fsw.Dispose();
+                }
+            }
+
+        }
+
+        private static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            FileSystemWatcher2 fsw = (FileSystemWatcher2)sender;
+            fsw.EnableRaisingEvents = false;
+            try
+            {
+                //LogText(e.Name + " >> " + fsw.ControlNumber.ToString("D4"), Color.White);
+                string newfilename = String.Format("{0:D4}_{1}", fsw.ControlNumber, e.Name);
+                string source = Path.Combine(fsw.Path, e.Name);
+                string destination = Path.Combine(fsw.Path, "FileWatcherBackups", newfilename);
+                File.Copy(source, destination);
+                LogText(e.Name + " >> " + fsw.ControlNumber.ToString("D4") + " copy OK", Color.Cyan);
+                fsw.ControlNumber++;
+            }
+            catch(Exception ex)
+            {
+                LogText("***FileSystemWatcher_Changed EXCEPTION***\n" + e.Name + " >> " + fsw.ControlNumber.ToString("D4") + "\n" + GetExceptionMessage(ex), Color.LightCoral);
+            }
+            finally
+            {
+                fsw.EnableRaisingEvents = true;
+            }
+           
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnSelect.Enabled = true;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            stopFlag = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            stopFlag = true;
+        }
+
+
+        public class FileSystemWatcher2 : FileSystemWatcher
+        {
+            public int ControlNumber = 0;
+            public object Tag = null;
+
+            public FileSystemWatcher2(string path) : base(path)
+            {
+                
+            }
+
+        }
+
+        public static string GetExceptionMessage(Exception exception, bool includeStack = false)
+        {
+            string message;
+            message = exception.GetType().FullName + ": " + exception.Message;
+            if (exception.InnerException != null)
+            {
+                message += "\n[InnerException] " + exception.InnerException.GetType().FullName + ": " + exception.InnerException.Message;
+            }
+            if (includeStack)
+                message += "\n" + exception.StackTrace;
+            return message;
+        }
+
+
+    }
+}
